5f745b8c6a25fb3eb90c0671edb407a633418f7d
CamiJi <aubertcam@gmail.com>
2016-02-15 17:29:37 +0100

    Structure nav bar + ajout Docs

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
+$(document).ready(function($) {
 
+	$('#btnDestination').click(function(e) {
+		e.preventDefault();
+
+		$navDestination = $('#navDestination');
+		if(!$navDestination.hasClass('active')) {
+			$navDestination.addClass('active');
+		}
+		else {
+			$navDestination.removeClass('active');
+		}
+	});
+
+	$('#navDestination').mouseout(function(e) {
+		e.preventDefault();
+	    $('#navDestination').hide();
+	});
+
+	$('main').click(function(e) {
+		e.preventDefault();
+	    $('#navDestination').hide();
+	});
+});
