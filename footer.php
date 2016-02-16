11acf842133b06cb6a82fdafe1acbd90ddf9f002
Atane <clement.brihi@gmail.com>
2016-02-16 15:45:05 +0100

    Footer
    Footer son père !

---
+++ footer.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title></title>
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<style>
+		body {
+			padding-top: 50px;
+			padding-bottom: 20px;
+		}
+	</style>
+	<link rel="stylesheet" href="css/bootstrap-theme.min.css">
+	
+	<link rel="stylesheet" href="css/main.css">
+
+
+	<script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+</head>
+<body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+            <![endif]-->
+
+
+            <footer>
+            	<div class="container">
+            		<div class="row">
+            			<div class="col-md-4">
+            				<a href="docs/CGU_2015.html">CGU</a>
+            				<p>&copy; Cliclao 2015</p>
+            			</div>
+            			<div class="col-md-4">
+            				<p>L'adresse</p>
+            				<a href="#">Contact</a>
+            			</div>
+            			<div class="col-md-4">
+            				<a href="#">Qui sommes nous ?</a>
+            			</div>
+            			<div class="col-md-4">
+            				<a target="_blank" title="follow me on twitter" href="http://www.twitter.com"><img alt="follow me on twitter" src="img/twitter_30x30.png" border=0></a>
+            				<a target="_blank" title="find us on Facebook" href="http://www.facebook.com"><img alt="follow me on facebook" src="img/facebook_30x30.png" border=0></a>
+            				<a target="_blank" title="find us on google plus" href="http://www.googleplus.com"><img alt="follow me on google plus" src="img/google.png" border=0></a>
+            			</div>
+            		</div>
+            	</div>
+            </footer>
+
+<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+
+<script src="js/vendor/bootstrap.min.js"></script>
+
+<script src="js/main.js"></script>
+</body>
+</html>
