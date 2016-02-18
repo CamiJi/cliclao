97bd539eadaa40e03c65eba5d8b9801f3ce16629
CamiJi <aubertcam@gmail.com>
2016-02-18 15:29:01 +0100

    index + footer + js

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
 
 
 
-
+ 
         <!-- Start Nav Bar  -->
         <nav id="nav-main" role="navigation" class="navbar navbar-default navbar-fixed-top">
 
@@ -35,8 +35,8 @@
               <div class="col-md-4"><p>Voyages individuels crées par des agences locales</p></div>
               <div class="col-md-4"><p><a>DEMANDE DE DEVIS</a></p></div>
               <div class="col-md-4"><p>ESPACE CLIENT : <a>SE CONNECTER </a> | <a>  S'INSCRIRE</a><p></div>            
-            </div><!--  collapsed navBar1Row -->
-          </div><!--  collapsed Container -->
+            </div><!--  end navBar1Row -->
+          </div><!--  end Container -->
 
           <div class="container">
             <div class="row">
@@ -51,100 +51,217 @@
                 </ul>     
 
               </div>          
-            </div><!--  collapsed navBar2Row -->
+            </div><!--  end navBar2Row -->
+          </div><!--  end Container -->
 
             
-
+            <!-- Start Menu des pays s'affichant avec le hover sur le #btnDestination "Les Destinations"  -->
             <div id="navDestination" class="container-country">
-              <div class="row">
-                <ul class="col-md-3">
-                  <li><a href="#" class="continentsListePays">EUROPE</a></li>
-                  <li><a href="#">Espagne</a></li>
-                  <li><a href="#">Italie</a></li>
-                  <li><a href="#">Irlande</a></li>
-                  <li><a href="#">Ecosse</a></li>
-                  <li><a href="#">Pays de Galle</a></li>
-                  <li><a href="#">Russie</a></li>
-                  <li><a href="#">Grèce</a></li>
-                  <li><a href="#">Croatie</a></li>
-                  <li><a href="#">Portugal</a></li>
-                  <li><a href="#">Irlande</a></li>
-                  <li><a href="#">Bulgarie</a></li>
-                  <li><a href="#">Norvège</a></li> 
-                  <li><a href="#">Islande</a></li>
-                  <li><a href="#">Suède</a></li>
-                  <li><a href="#">Finlande</a></li>
-                  <li><a href="#">Turquie</a></li>
-                </ul>
-                <ul class="col-md-3">
-                  <li><a href="#" class="continentsListePays">AMERIQUES</a></li>
-                  <li><a href="#">Argentine</a></li>
-                  <li><a href="#">Brésil </a></li>
-                  <li><a href="#">Cuba</a></li>
-                  <li><a href="#">Pérou</a></li>
-                  <li><a href="#">Bolivie</a></li>
-                  <li><a href="#">Equateur</a></li>
-                  <li><a href="#">Colombie</a></li>
-                  <li><a href="#">Costa Rica</a></li>
-                  <li><a href="#">République dominicaine</a></li>
-                  <li><a href="#">Mexique </a></li>
-                  <li><a href="#">Guatemala</a></li>
-                  <li><a href="#">États-Unis  </a></li>
-                  <li><a href="#">Canada  </a></li>
-                </ul>
-                <ul class="col-md-3">
-                  <li><a href="#" class="continentsListePays">ASIE</a></li>
-                  <li><a href="#">Inde</a></li>
-                  <li><a href="#">Indonésie</a></li>
-                  <li><a href="#">Japon</a></li> 
-                  <li><a href="#">Singapour</a></li>
-                  <li><a href="#">Thaïlande</a></li>
-                  <li><a href="#">Corée du Sud</a></li>
-                  <li><a href="#">Malaisie</a></li>
-                  <li><a href="#">Hong Kong</a></li>
-                  <li><a href="#">Chine </a></li>
-                  <li><a href="#">Vietnam</a></li>
-                  <li><a href="#">Sri Lanka</a></li>
-                  <li><a href="#">Mongolie</a></li>
-                  <li><a href="#">Népal</a></li>
-                  <li><a href="#">Birmanie</a></li>
-                  <li><a href="#">Iran</a></li>
-                  <li><a href="#">Laos</a></li>
-                  <li><a href="#">Cambodge</a></li>
-                </ul>
-                <ul class="col-md-3">
-                  <li><a href="#" class="continentsListePays">AFRIQUE</a></li>
-                  <li><a href="#">Sénégal</a></li>
-                  <li><a href="#">Maroc </a></li>
-                  <li><a href="#">Tunisie</a></li>
-                  <li><a href="#">Égypte</a></li>
-                  <li><a href="#">Rwanda</a></li> 
-                  <li><a href="#">Kenya</a></li>
-                  <li><a href="#">Tanzanie</a></li>
-                  <li><a href="#">Afrique du Sud</a></li>
-                  <li><a href="#">Madagascar</a></li>
+              <div class="container">
+                <div class="row">
+                  <ul class="col-md-3">
+                    <li><a href="#" class="continentsListePays">EUROPE</a></li>
+                    <li><a href="#">Espagne</a></li>
+                    <li><a href="#">Italie</a></li>
+                    <li><a href="#">Irlande</a></li>
+                    <li><a href="#">Ecosse</a></li>
+                    <li><a href="#">Pays de Galle</a></li>
+                    <li><a href="#">Russie</a></li>
+                    <li><a href="#">Grèce</a></li>
+                    <li><a href="#">Croatie</a></li>
+                    <li><a href="#">Portugal</a></li>
+                    <li><a href="#">Irlande</a></li>
+                    <li><a href="#">Bulgarie</a></li>
+                    <li><a href="#">Norvège</a></li> 
+                    <li><a href="#">Islande</a></li>
+                    <li><a href="#">Suède</a></li>
+                    <li><a href="#">Finlande</a></li>
+                    <li><a href="#">Turquie</a></li>
+                  </ul>
+                  <ul class="col-md-3">
+                    <li><a href="#" class="continentsListePays">AMERIQUES</a></li>
+                    <li><a href="#">Argentine</a></li>
+                    <li><a href="#">Brésil </a></li>
+                    <li><a href="#">Cuba</a></li>
+                    <li><a href="#">Pérou</a></li>
+                    <li><a href="#">Bolivie</a></li>
+                    <li><a href="#">Equateur</a></li>
+                    <li><a href="#">Colombie</a></li>
+                    <li><a href="#">Costa Rica</a></li>
+                    <li><a href="#">République dominicaine</a></li>
+                    <li><a href="#">Mexique </a></li>
+                    <li><a href="#">Guatemala</a></li>
+                    <li><a href="#">États-Unis  </a></li>
+                    <li><a href="#">Canada  </a></li>
+                  </ul>
+                  <ul class="col-md-3">
+                    <li><a href="#" class="continentsListePays">ASIE</a></li>
+                    <li><a href="#">Inde</a></li>
+                    <li><a href="#">Indonésie</a></li>
+                    <li><a href="#">Japon</a></li> 
+                    <li><a href="#">Singapour</a></li>
+                    <li><a href="#">Thaïlande</a></li>
+                    <li><a href="#">Corée du Sud</a></li>
+                    <li><a href="#">Malaisie</a></li>
+                    <li><a href="#">Hong Kong</a></li>
+                    <li><a href="#">Chine </a></li>
+                    <li><a href="#">Vietnam</a></li>
+                    <li><a href="#">Sri Lanka</a></li>
+                    <li><a href="#">Mongolie</a></li>
+                    <li><a href="#">Népal</a></li>
+                    <li><a href="#">Birmanie</a></li>
+                    <li><a href="#">Iran</a></li>
+                    <li><a href="#">Laos</a></li>
+                    <li><a href="#">Cambodge</a></li>
+                  </ul>
+                  <ul class="col-md-3">
+                    <li><a href="#" class="continentsListePays">AFRIQUE</a></li>
+                    <li><a href="#">Sénégal</a></li>
+                    <li><a href="#">Maroc </a></li>
+                    <li><a href="#">Tunisie</a></li>
+                    <li><a href="#">Égypte</a></li>
+                    <li><a href="#">Rwanda</a></li> 
+                    <li><a href="#">Kenya</a></li>
+                    <li><a href="#">Tanzanie</a></li>
+                    <li><a href="#">Afrique du Sud</a></li>
+                    <li><a href="#">Madagascar</a></li>
+                    
+                    <li></br></li>
 
-                  <li><a href="#" class="divider"></a></li>
+                    <li><a href="#" class="continentsListePays">OCEANIE</a></li>
+                    <li><a href="#">Australie</a></li>
+                    <li><a href="#">Nouvelle Zélande</a></li>
+                    <li><a href="#">Polynésie</a></li>
 
-                  <li><a href="#" class="continentsListePays">OCEANIE</a></li>
-                  <li><a href="#">Australie</a></li>
-                  <li><a href="#">Nouvelle Zélande</a></li>
-                  <li><a href="#">Polynésie</a></li>
-
-                </ul>
-              </div>
-            </div>
-          </div><!--  collapsed Container -->
-        </nav><!-- Collapsed Nav Bar -->
+                  </ul>
+                </div> <!-- End Row Liste des pays -->
+              </div> <!-- End Container Pays -->
+            </div> <!-- End Nav Destination -->
+        </nav><!-- end Nav Bar -->
 
         <main>
-            <div id="video">
+
+
+
+
+
+            <!-- Start Row Video -->
+            <div class="row" id="rowSearch">
+              <!-- TODO intgrer un background video -->
               
+
+              <div class="container">
+                <div class="row">
+                  <div class="col-md-6 col-md-offset-1">
+                   <h1>Créer votre voyage sur mesure avec une agence locale</h1>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-md-6 col-md-offset-1">
+                    <div class="input-group">
+                      <span class="input-group-btn">
+                        <button class="btn btn-default" type="button">Go!</button>
+                      </span>
+                      <input type="text" class="form-control" placeholder="Dans quel pays désirez-vous voyager?">
+                    </div><!-- /input-group -->
+                  </div>
+                </div>
+
+              </div>
+              <!-- glyphicon glyphicon-search pour l'input -->
+
+
+
+            </div> <!-- End rowVideo -->
+
+
+
+
+
+          <!-- Start Main Container -->
+          <div class="container"> 
+            <!-- Start Row Thumbnails 1 -->
+            <div class="row" id=ThumbnailsRow1>
+              <div class = "col-sm-6 col-md-3">
+                <div class="thumbnail tile tile-medium tile-teal">
+                  <a href="#" >
+                    <h1>Tuile1</h1>
+                  </a>                  
+                </div>
+              </div>
+              <div class = "col-sm-6 col-md-3">
+                <div class="thumbnail tile tile-medium tile-teal">
+                  <a href="#" >
+                    <h1>Tuile2</h1>
+                  </a>                  
+                </div>
+              </div>
+              <div class = "col-sm-6 col-md-3">
+                <div class="thumbnail tile tile-medium tile-teal">
+                  <a href="#" >
+                    <h1>Tuile2</h1>
+                  </a>                  
+                </div>
+              </div>
+            </div> <!-- ThumbnailsRow1 -->
+
+
+
+
+            <!-- Start Row Thumbnails 2 -->
+            <div class="row" id=ThumbnailsRow2>
+              <div class = "col-sm-6 col-md-3">
+                <div class="thumbnail tile tile-medium tile-teal">
+                  <a href="#" >
+                    <h1>Tuile4</h1>
+                  </a>                  
+                </div>
+              </div>
+              <div class = "col-sm-6 col-md-3">
+                <div class="thumbnail tile tile-medium tile-teal">
+                  <a href="#" >
+                    <h1>Tuile5</h1>
+                  </a>                  
+                </div>
+              </div>
+              <div class = "col-sm-6 col-md-3">
+                <div class="thumbnail tile tile-medium tile-teal">
+                  <a href="#" >
+                    <h1>Tuile6</h1>
+                  </a>                  
+                </div>
+              </div>
+
+            </div> <!-- End ThumbnailsRow2 -->
+          </div> <!-- End container main -->
+        </main><!--   End Main -->
+
+
+
+
+        <!-- Start Footer -->
+        <footer>
+          <div class="container">
+            <div class="row">
+              <div class="col-md-4">
+                <a href="docs/CGU_2015.html">CGU</a>
+                <p>&copy; Cliclao 2015</p>
+              </div>
+              <div class="col-md-4">
+                <p>L'adresse</p>
+                <a href="#">Contact</a>
+              </div>
+              <div class="col-md-4">
+                <a href="#">Qui sommes nous ?</a>
+              </div>
+              <div class="col-md-4">
+                <a target="_blank" title="follow me on twitter" href="http://www.twitter.com"><img alt="follow me on twitter" src="img/twitter_30x30.png" border=0></a>
+                <a target="_blank" title="find us on Facebook" href="http://www.facebook.com"><img alt="follow me on facebook" src="img/facebook_30x30.png" border=0></a>
+                <a target="_blank" title="find us on google plus" href="http://www.googleplus.com"><img alt="follow me on google plus" src="img/google.png" border=0></a>
+              </div>
             </div>
-        </main><!--   Collapsed Main -->
-
-
-        <?php include(__DIR__.'/footer.php'); ?>
+          </div>
+        </footer> <!-- End Footer -->
 
 
 
