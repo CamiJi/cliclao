344850f006d2a6db1f558c63ec30332f356ae112
CamiJi <aubertcam@gmail.com>
2016-02-20 19:56:55 +0100

    integraIntegration Itineraire.php

--- index.php
+++ index.php
@@ -14,6 +14,8 @@
 
         <link rel="stylesheet" href="css/bootstrap.min.css">
         <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+        <!-- Chargement de FontAwesome https://fortawesome.github.io/Font-Awesome/get-started/ -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="css/main.css">
 
         <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
--- itineraires.php
+++ itineraires.php
@@ -14,6 +14,8 @@
 
         <link rel="stylesheet" href="css/bootstrap.min.css">
         <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+        <!-- Chargement de FontAwesome https://fortawesome.github.io/Font-Awesome/get-started/ -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="css/main.css">
 
         <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
@@ -23,6 +25,60 @@
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
 
+
+    <main>
+      <div class="container" id="mainContainer">
+
+
+
+
+        <div class="col-xs-12 col-md-8" id="colMainItineraire">
+          <div id="divImgItineraire">
+            <img src="img/vc.jpg" id="imgItineraire">
+          </div> <!-- end divImgItineraire -->
+          <div id="divNomItineraire">
+            <h3>Canada : Québec en famille</h3>
+          </div> <!-- End divNomItineraire  -->
+          <div id="divDescriptionItineraire">
+            <p id="descriptionItineraire">Au Québec, il y a tant de choses à faire et à voir que c’est une destination idéale à découvrir en famille. Les enfants vont adorer dormir dans un tipi et apprendre l’histoire des amérindiens, observer les animaux sauvages dans leur habitat naturel : castor, orignal, ours noir, en découvrir d’autres au fabuleux zoo sauvage de St Félicien, aller faire du kayak ou de l’accrobranche avec des vues panoramiques sur le fjord du Saguenay, nourrir les animaux de la ferme dans la région de Tadoussac pour enfin partir sur l’eau pour observer les géants des mers : les baleines à bosse et autres mammifères marins. Vos enfants vous en reparleront longtemps de ce voyage ! Les hébergements ont été sélectionnés en fonction des préférences des enfants.</p>
+          </div> <!-- End #divDescriptionItineraire -->          
+        </div> <!-- End colMainItineraire -->
+
+
+
+
+
+
+
+
+
+
+
+        <div class="col-xs-6 col-md-4" id="colCaractItineraires">
+          <div id="divCaracteristiquesItineraire">
+            <div id="divPrixItineraire">
+              <p id="prixItineraire">à partir de <br/>659 € par personne<br/> hors vols</p>
+            </div> <!-- End#divPrixItineraire -->
+            <div id="divDifficulteItineraire">
+              <p id="difficulteItineraire">Niveau de Dificulté </br><i class="fa fa-hand-scissors-o"></i> <!-- TODO: on repetera ce signe le nombre de niveau de difficulté -->  </p>
+            </div> <!-- End#divDifficulteItineraire -->
+            <div id="divThemesItineraire">
+              <i class="fa fa-anchor"></i>
+              <i class="fa fa-car"></i>
+              <i class="fa fa-ship"></i>
+              <i class="fa fa-bicycle"></i>            
+            </div> <!-- End divThemesItineraire -->
+            <button type="button" class="btn btn-success">Demandez votre devis</button> </br>
+            <button type="button" class="btn btn-default">Ajoutez aux favoris</button>
+            <div id="divApiGMItineraire">
+              <img src="img/mapCanada.jpg" alt="" id="apiGMItineraire">             
+            </div> <!-- end #divApiGMItineraire -->
+
+            
+          </div> <!-- End #divCaracteristiquesItineraire -->         
+        </div> <!-- End colCaractItineraires -->
+      </div> <!-- end MainContainer -->
+    </main>
       
   
 <!-- ********************************************************************************************************************************-->
